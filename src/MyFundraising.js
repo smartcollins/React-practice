e9ee239781d72b9d5579e7da75f1080e1e08b899
collins <kevin1520smart@gmail.com>
2022-06-10 21:28:41 +1100

    fundraising section still in progress

--- src/MyFundraising.js
+++ src/MyFundraising.js
@@ -7,33 +7,69 @@ function MyFundraising(){
 	function onPending(){
 		setPending(true)
 	}
+
+	const style={
+		color: "white",
+		backgroundColor: "#35e01f",
+		borderWidth: "2px"
+	}
+
+	const style2={
+		backgroundColor: "transparent",
+		color: "35e01f"
+	}
 	
 
 	return(
 		<div>
+			<div className="bookmark-top">
+				<div className="back">
+					<UsersThree size={30} color="#35e01f"/>
+					<h1>My Fundraising</h1>
+				</div>
+				<button className="dot3">
+					<ArchiveBox size={30} color="#35e01f" weight="duotone" />
+				</button>
+			</div>
+			<div className="myfund-top">
+				<h3>MyFundraising</h3>
+				<h3>Activity</h3>
+			</div>
+			<div className="urgent-btn">
+				<button>All(25)</button>
+				<button>Ongoing(3)</button>
+				<button>Past(22)</button>
+				<button onClick={onPending} style={pending?style:style2}>Pending(1)</button>
+			</div>
 			{
 				pending ?
-				<div></div>:
-				<div>
-					<div className="bookmark-top">
-						<div className="back">
-							<UsersThree size={30} color="#35e01f"/>
-							<h1>My Fundraising</h1>
+				<div className="pending">
+					<div className="mydonation-donation">
+						<div className="search-result">
+							<div className="search-img">
+								<div className="urgent-mark">
+									<BookmarkSimple size={20} color="#fafafa" weight="duotone" />
+								</div>
+							</div>
+							<div className="search-detail">
+								<div className="search-note">
+									<h3>Help African Children's Overcome Starvation</h3>
+									<div className="search-fund">
+										<p><span>$7,560</span> fund required</p>
+									</div>
+								</div>
+								<div className="urgent-bar"></div>
+								<div className="review">
+									<p><span>30</span> Days</p>
+								</div>
+							</div>
 						</div>
-						<button className="dot3">
-							<ArchiveBox size={20} color="#35e01f" weight="bold" />
-						</button>
+						<div className="review-end">
+							<p>Waiting for review ...</p>
+						</div>
 					</div>
-					<div className="myfund-top">
-						<h3>MyFundraising</h3>
-						<h3>Activity</h3>
-					</div>
-					<div className="urgent-btn">
-						<button>All(25)</button>
-						<button>Ongoing(3)</button>
-						<button>Past(22)</button>
-						<button>Pending(1)</button>
-					</div>
+				</div>:
+				<div>
 					<div className="plus">
 						<PlusCircle size={70} color="#35e01f" weight="fill" />
 					</div>
