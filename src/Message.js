2b909bab835b4dbd2d73c3f13d7ac20e46437b4b
collins <kevin1520smart@gmail.com>
2022-06-22 19:08:50 +0100

    message-block section completed

--- src/Message.js
+++ src/Message.js
@@ -7,6 +7,7 @@ import {ArrowLeft,DotsThreeVertical,Circle,PaperPlaneRight,Prohibit} from "phosp
 function Message(){
 	const [back,setBack] = useState(false)
 	const [send,setSend] = useState(false)
+	const [block,setBlock] = useState(false)
 
 	function onBack(){
 		setBack(true)
@@ -14,6 +15,14 @@ function Message(){
 
 	function onSend(){
 		setSend(true)
+	}
+
+	function onBtnblock(){
+		setBlock(true)
+	}
+
+	function onCancel(){
+		setBlock(false)
 	}
 
 	const style ={
@@ -27,53 +36,63 @@ function Message(){
 				back?
 				<Inbox/>:
 				<div className="message">
-					<div className="bookmark-top">
-						<div className="back">
-							<ArrowLeft size={30} color="#1EBA60" onClick={onBack} />
-							<h3>Jane Cooper</h3>
-						</div>
-						<div className="prayer2-top">
-							<button className="dot3">
-								<Prohibit size={20} color="#1EBA60" />
-							</button>
-							<button className="dot3">
-								<DotsThreeVertical size={20} color="#1EBA60" />
-							</button>
+					<div style={{visibility:block?"visible":"hidden"}} className="share-main">
+						<Prohibit size={30} color="#1EBA60" />
+						<p>Block <span>Kathryn Murphy</span> ?</p>
+						<div className="great-btn">
+							<button onClick={onCancel}>Cancel</button>
+							<button onClick={onBack}>Yes, Block</button>
 						</div>
 					</div>
-					<div>
-						<div className="recent">
-							<h3>Today</h3>
+					<div className={block?"success":"message"}>
+						<div className="bookmark-top">
+							<div className="back">
+								<ArrowLeft size={30} color="#1EBA60" onClick={onBack} />
+								<h3>Jane Cooper</h3>
+							</div>
+							<div className="prayer2-top">
+								<button onClick={onBtnblock} className="dot3">
+									<Prohibit size={20} color="#1EBA60" />
+								</button>
+								<button className="dot3">
+									<DotsThreeVertical size={20} color="#1EBA60" />
+								</button>
+							</div>
 						</div>
-						<div className="message-txt">
-							<p>Hello, good morning!😊 </p>
-							<p>09:41</p>
+						<div>
+							<div className="recent">
+								<h3>Today</h3>
+							</div>
+							<div className="message-txt">
+								<p>Hello, good morning!😊 </p>
+								<p>09:41</p>
+							</div>
+							<div className="message-txt">
+								<p>I am interested in making a donation. May i know what the donation program you have just published is for?😊 </p>
+								<p>09:41</p>
+							</div>
+							<div className="response">
+								<p>Hi, good afternoon. Donations will be distributed to flood victims in Surabuya.You can see detailed information in my post</p>
+								<p>09:41</p>
+							</div>
+							<div className="message-txt">
+								<p>Great, thanks a lot for the information</p>
+								<p>09:41</p>
+							</div>
+							<div className="message-txt">
+								<p>i will make a donation as soon as possible after this</p>
+								<p>09:41</p>
+							</div>
+							<div className="response">
+								<p>You're welcome. Thank you for the donations that will be given.</p>
+								<p>09:41</p>
+							</div>
+							<div className="send" onClick={onSend} style={style}>
+								<input placeholder="Type message ..." />
+								<PaperPlaneRight size={25} color="#0da542" weight="bold" />
+							</div>
 						</div>
-						<div className="message-txt">
-							<p>I am interested in making a donation. May i know what the donation program you have just published is for?😊 </p>
-							<p>09:41</p>
-						</div>
-						<div className="response">
-							<p>Hi, good afternoon. Donations will be distributed to flood victims in Surabuya.You can see detailed information in my post</p>
-							<p>09:41</p>
-						</div>
-						<div className="message-txt">
-							<p>Great, thanks a lot for the information</p>
-							<p>09:41</p>
-						</div>
-						<div className="message-txt">
-							<p>i will make a donation as soon as possible after this</p>
-							<p>09:41</p>
-						</div>
-						<div className="response">
-							<p>You're welcome. Thank you for the donations that will be given.</p>
-							<p>09:41</p>
-						</div>
-						<div className="send" onClick={onSend} style={style}>
-							<input placeholder="Send your prayers..." />
-							<PaperPlaneRight size={25} color="#0da542" weight="bold" />
-						</div>
-					</div>
+					</div>	
 				</div>
 			}
 		</div>
