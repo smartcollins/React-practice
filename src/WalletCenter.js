b3d3583b0da26bed28ecd6315c37ecc601d47a7b
collins <kevin1520smart@gmail.com>
2022-06-23 15:08:07 +0100

    activity section in Wallet center complete

--- src/WalletCenter.js
+++ src/WalletCenter.js
@@ -1,6 +1,6 @@
 import {useState} from "react"
 import Userprofile from "./Userprofile"
-import {ArrowLeft,DotsThreeVertical,ArrowFatLineUp,ArrowFatLineDown,CheckCircle,XCircle} from "phosphor-react"
+import {ArrowLeft,DotsThreeVertical,ArrowFatLineUp,ArrowFatLineDown,CheckCircle,Bell,XCircle} from "phosphor-react"
 
 function WalletCenter(){
 	const [back,setBack] = useState(false)
@@ -21,7 +21,7 @@ function WalletCenter(){
 							<h1>Wallet Center</h1>
 						</div>
 						<button className="dot3">
-							<DotsThreeVertical size={30} color="#" />
+							<DotsThreeVertical size={30} color="#1EBA60" />
 						</button>
 					</div>
 					<div>
@@ -39,6 +39,42 @@ function WalletCenter(){
 							<ArrowFatLineDown size={20} color="#1EBA60" />
 							<p>Withdraw</p>
 						</button>
+					</div>
+					<div className="notify">
+						<h3>Activity</h3>
+						<p>Today, December 25 2023</p>
+					</div>
+					<div className="notify">
+						<div className="notify-bar">
+							<button className="notify-logo">
+								<CheckCircle size={20} color="#ffffff" weight="fill"/>
+							</button>
+							<div>
+								<h5>Top up Successful</h5>
+								<p>You have successsfully top up your wallet in the amount of $100</p>
+							</div>
+						</div>
+						<div className="notify-bar">
+										<button className="notify-logo cancel">
+											<XCircle size={20} color="#ffffff" weight="fill"/>
+										</button>
+										<div>
+											<h5>Donation Cancelled!</h5>
+											<p>You have cancel donation for victims of natural disasters</p>
+										</div>
+
+									</div>
+									<p>Yesterday, December 24 2023</p>
+									<div className="notify-bar">
+										<button className="notify-logo new">
+											<Bell size={20} color="#ffffff" weight="fill" />
+										</button>
+										<div>
+											<h5>New Feature Available</h5>
+											<p>You can now invite friends to join donation with you</p>
+										</div>
+									</div>
+
 					</div>
 				</div>
 			}
