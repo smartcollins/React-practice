da1bd7b0048b618f42724233c363d71ad90a41c1
collins <kevin1520smart@gmail.com>
2022-06-27 16:24:19 +0100

    created all subordinates of help section

---
+++ src/Faqs.js
@@ -0,0 +1,30 @@
+import {useState} from "react"
+import Help from "./Help"
+import {ArrowLeft} from "phosphor-react"
+
+function Faqs(){
+	const [back,setBack] = useState(false)
+
+	function onBack(){
+		setBack(true)
+	}
+
+	return(
+		<div>
+			{
+				back?
+				<Help/>:
+				<div>
+					<div className="bookmark-top">
+						<div className="back">
+							<ArrowLeft size={50} color="#1EBA60" onClick={onBack} />
+							<h1>Faqs</h1>
+						</div>
+					</div>
+				</div>
+			}
+		</div>
+		)
+}
+
+export default Faqs;
--- src/Help.js
+++ src/Help.js
@@ -1,19 +1,60 @@
 import {useState} from "react"
 import Setting from "./Setting"
+import Faqs from "./Faqs"
+import Contact from "./Contact"
+import Terms from "./Terms"
+import Privacy from "./Privacy"
+import About from "./About"
 import {ArrowLeft,InstagramLogo,TwitterLogo,GlobeHemisphereEast,YoutubeLogo,CaretRight} from "phosphor-react"
 
 function Help(){
 	const [back,setBack] = useState(false)
-
+	const [faq,setFaq] = useState(false)
+	const [contact,setContact] = useState(false)
+	const [term,setTerm] = useState(false)
+	const [privacy,setPrivacy] = useState(false)
+	const [about,setAbout] = useState(false)
+	
 	function onBack(){
 		setBack(true)
 	}
+
+	function onFaq(){
+		setFaq(true)
+	}
+
+	function onContact(){
+		setContact(true)
+	}
+
+	function onTerm(){
+		setTerm(true)
+	}
+
+	function onPrivacy(){
+		setPrivacy(true)
+	}
+
+	function onAbout(){
+		setAbout(true)
+	}
+
 
 	return(
 		<div>
 			{
 				back?
 				<Setting/>:
+				faq?
+				<Faqs/>:
+				contact?
+				<Contact/>:
+				term?
+				<Terms/>:
+				privacy?
+				<Privacy/>:
+				about?
+				<About/>:
 				<div>
 					<div className="bookmark-top">
 						<div className="back">
@@ -39,23 +80,23 @@ function Help(){
 							YouTube
 						</button>
 					</div>
-					<div className="setting-mid">
+					<div onClick={onFaq} className="setting-mid">
 						<p>FAQs</p>
 						<CaretRight size={20} color="#1EBA60" />
 					</div>
-					<div className="setting-mid">
+					<div onClick={onContact} className="setting-mid">
 						<p>Contact us</p>
 						<CaretRight size={20} color="#1EBA60" />
 					</div>
-					<div className="setting-mid">
+					<div onClick={onTerm} className="setting-mid">
 						<p>Terms & Conditions</p>
 						<CaretRight size={20} color="#1EBA60" />
 					</div>
-					<div className="setting-mid">
+					<div onClick={onPrivacy} className="setting-mid">
 						<p>Privacy Policy</p>
 						<CaretRight size={20} color="#1EBA60" />
 					</div>
-					<div className="setting-mid">
+					<div onClick={onAbout} className="setting-mid">
 						<p>About Us</p>
 						<CaretRight size={20} color="#1EBA60" />
 					</div>
