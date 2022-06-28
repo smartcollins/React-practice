a79ce18f1d9ece417bab06f6d125cc7eb9e566e7
collins <kevin1520smart@gmail.com>
2022-06-28 12:26:43 +0100

    about us section completed

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import {useState} from "react"
 import Help from "./Help"
-import {ArrowLeft} from "phosphor-react"
+import {ArrowLeft,Users} from "phosphor-react"
 
 function About(){
 	const [back,setBack] = useState(false)
@@ -14,12 +14,57 @@ function About(){
 			{
 				back?
 				<Help/>:
-				<div>
+				<div className="about">
 					<div className="bookmark-top">
 						<div className="back">
 							<ArrowLeft size={50} color="#1EBA60" onClick={onBack} />
 							<h1>About</h1>
 						</div>
+					</div>
+					<div className="about-btn">
+						<button>
+							<Users size={50} color="#ffffff" weight="duotone" />
+						</button>
+						<h3>Wecare</h3>
+					</div>
+					<h3>We Focus on Digital Charity</h3>
+					<div className="terms">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Sed convallis tellus eu imperdiet consequat. Quisque quis lectus sed est dignissim mollis.
+							Quisque luctus est ac nisl cursus, vel interdum est condimentum.
+							Nunc tincidunt tempor ipsum eu pulvinar. Cras lobortis leo sit amet condimentum aliquet.
+							Pellentesque laoreet vulputate mauris, a tempor nunc vehicula eget.
+							Quisque a nunc quis diam aliquet viverra.
+							Nam sagittis accumsan turpis, at pharetra ex sagittis ac. 
+							Quisque luctus est ac nisl cursus, vel interdum est condimentum.
+							Nunc tincidunt tempor ipsum eu pulvinar. Cras lobortis leo sit amet condimentum aliquet.
+							Pellentesque laoreet vulputate mauris, a tempor nunc vehicula eget.
+							Quisque a nunc quis diam aliquet viverra.							
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Sed convallis tellus eu imperdiet consequat. Quisque quis lectus sed est dignissim mollis.
+							Quisque luctus est ac nisl cursus, vel interdum est condimentum.
+							Nunc tincidunt tempor ipsum eu pulvinar. Cras lobortis leo sit amet condimentum aliquet.
+							Pellentesque laoreet vulputate mauris, a tempor nunc vehicula eget.
+							Quisque a nunc quis diam aliquet viverra.
+							Nam sagittis accumsan turpis, at pharetra ex sagittis ac. 
+							Quisque luctus est ac nisl cursus, vel interdum est condimentum.
+							Nunc tincidunt tempor ipsum eu pulvinar. Cras lobortis leo sit amet condimentum aliquet.
+							Pellentesque laoreet vulputate mauris, a tempor nunc vehicula eget.
+							Quisque a nunc quis diam aliquet viverra.							
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Sed convallis tellus eu imperdiet consequat. Quisque quis lectus sed est dignissim mollis.
+							Quisque luctus est ac nisl cursus, vel interdum est condimentum.
+							Nunc tincidunt tempor ipsum eu pulvinar. Cras lobortis leo sit amet condimentum aliquet.
+							Pellentesque laoreet vulputate mauris, a tempor nunc vehicula eget.
+							Quisque a nunc quis diam aliquet viverra.
+							Nam sagittis accumsan turpis, at pharetra ex sagittis ac. 
+							Quisque luctus est ac nisl cursus, vel interdum est condimentum.
+						</p>
 					</div>
 				</div>
 			}
