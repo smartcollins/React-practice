c4fd7a7cdfcd5c0586f3db7912f911d51e9ad5f9
collins <kevin1520smart@gmail.com>
2022-06-28 11:27:42 +0100

    contact section completed

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import {useState} from "react"
 import Help from "./Help"
-import {ArrowLeft} from "phosphor-react"
+import {ArrowLeft,EnvelopeSimple,PaperPlaneRight} from "phosphor-react"
 
 function Contact(){
 	const [back,setBack] = useState(false)
@@ -14,13 +14,46 @@ function Contact(){
 			{
 				back?
 				<Help/>:
-				<div>
+				<div className="contact">
 					<div className="bookmark-top">
 						<div className="back">
 							<ArrowLeft size={50} color="#1EBA60" onClick={onBack} />
 							<h1>Contact</h1>
 						</div>
 					</div>
+					<div className="fund-detail">
+						<div className="user-detail">
+							<p>Full Name <span>*</span></p>
+							<div className="user-input">
+								<input placeholder="Full name"/>
+							</div>
+							<p>Email <span>*</span></p>
+							<div className="user-input">
+								<input type="email" placeholder="Email"/>
+								<EnvelopeSimple size={20} color="#7d7d7d" weight="duotone" />
+							</div>
+							<div>
+								<div className="coming-top">
+									<p><label for="mytext">Fund Usage <span>*</span></label></p>
+									<p>max word 250</p>
+								</div>
+								<textarea id="mytext" name="mytext" rows="2" cols="50">
+								lorem ipsum dolor sit amet, consectetur adipiscing elit,
+									Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+									Ut enim ad minim veniam,quis nostrad.
+									Ut labore et dolore magna aliqua. Ut enim ad minim.
+								At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
+								</textarea>
+							</div>
+						</div>
+						<div>
+							<button className="nxt">
+								<p>Send Messages</p>
+								<PaperPlaneRight size={20} color="#ffffff" />
+							</button>
+						</div>
+					</div>
+					
 				</div>
 			}
 		</div>
